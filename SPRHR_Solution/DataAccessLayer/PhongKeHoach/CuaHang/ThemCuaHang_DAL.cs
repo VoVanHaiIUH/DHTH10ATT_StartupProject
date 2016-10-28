beb50d8df099c156de0bc6eba466f447a4b486a9
NguyenNgocHoa1996 <hoait1996@gmail.com>
2016-10-28 19:34:21 +0700

    Add

---
+++ SPRHR_Solution/DataAccessLayer/PhongKeHoach/CuaHang/ThemCuaHang_DAL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities.PhongKeHoach;
+
+namespace DataAccessLayer.PhongKeHoach.CuaHang
+{
+    public class ThemCuaHang_DAL
+    {
+        SPRHR_SolutionDataContext db = new SPRHR_SolutionDataContext();
+        public bool CheckCoKhongCuaHang(string MaCuaHang)
+        {
+            var c = from i in db.DanhMucCuaHangs
+                    where i.MaCuaHang == MaCuaHang
+                    select i;
+            if(c.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+        // Delete CuaHang
+        // Fix Thong Tin CuaHang
+        public bool DeleteCuaHang(string MaCuaHang)
+        {
+            DanhMucCuaHang CH = db.DanhMucCuaHangs.Where(x => x.MaCuaHang == MaCuaHang).FirstOrDefault();
+            if(CH != null)
+            {
+                db.DanhMucCuaHangs.DeleteOnSubmit(CH);
+                db.SubmitChanges();
+                return true;
+            }
+            return false;
+        }
+        public void FixCuaHang(string MaCuaHang,string TenCuaHang,string DiaChi,string SoDienThoai,int TinhTrang)
+        {
+
+        }
+    }
+}
---
+++ SPRHR_Solution/DataAccessLayer/PhongKeHoach/PhieuDeNghiXuatKho/LapPhieuDeNghiXuatKho_DAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities.PhongKeHoach;
+
+namespace DataAccessLayer.PhongKeHoach.PhieuDeNghiXuatKho
+{
+    public class LapPhieuDeNghiXuatKho_DAL
+    {
+        SPRHR_SolutionDataContext db = new SPRHR_SolutionDataContext();
+        public bool CheckCoKhongPhieuDNXK(string SoPhieu)
+        {
+            var c = from i in db.PhieuDNXKs
+                    where i.MaPhieuDNXK == SoPhieu
+                    select i;
+            if(c.Any())
+            {
+                return true;
+            }
+            return false;
+        }
+        // Tao PDNXK
+        // Tao chi tiet PDNXK
+        // Delete PDNXK
+        // Delete chi tiet PDNXK
+        // ....
+        // ....
+        //.........>>
+        public int InsertPDNXK(ePhieuDeNghiXuatKho Phieu)
+        {
+            
+        }
+        public int InsertPDNXKChiTiet(eChiTietPhieuDeNghiXuatKho Chitiet)
+        {
+
+        }
+        public bool DeLetePDNXK(string SoPhieu)
+        {
+
+        }
+        public bool DeletePDNXKChiTiet(string SoPhieu, string MaSP)
+        {
+
+        }
+    }
+}
