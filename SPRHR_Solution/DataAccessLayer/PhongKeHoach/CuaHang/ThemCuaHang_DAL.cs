3d1925fa0262dc8e9628393686868df1f9b49942
NguyenNgocHoa1996 <hoait1996@gmail.com>
2016-10-28 19:46:25 +0700

    Add

--- SPRHR_Solution/DataAccessLayer/PhongKeHoach/CuaHang/ThemCuaHang_DAL.cs
+++ SPRHR_Solution/DataAccessLayer/PhongKeHoach/CuaHang/ThemCuaHang_DAL.cs
@@ -21,8 +21,31 @@ namespace DataAccessLayer.PhongKeHoach.CuaHang
             }
             return false;
         }
+        // Insert CuaHang
         // Delete CuaHang
         // Fix Thong Tin CuaHang
+        public bool InsertCuaHang(eCuaHang CH)
+        {
+            var c = from i in db.DanhMucCuaHangs
+                    where i.MaCuaHang == CH.MaCuaHang
+                    select i;
+            if(c.Any())
+            {
+                return false;
+            }
+            else
+            {
+                DanhMucCuaHang CuaHang = new DanhMucCuaHang();
+                CuaHang.MaCuaHang = CH.MaCuaHang;
+                CuaHang.TenCuaHang = CH.TenCuaHang;
+                CuaHang.DiaChi = CH.DiaChi;
+                CuaHang.SoDienThoai = CH.SoDienThoai;
+                CuaHang.TinhTrang = Convert.ToInt32(CH.SoDienThoai);
+                db.DanhMucCuaHangs.InsertOnSubmit(CuaHang);
+                db.SubmitChanges();
+                return true;
+            }
+        }
         public bool DeleteCuaHang(string MaCuaHang)
         {
             DanhMucCuaHang CH = db.DanhMucCuaHangs.Where(x => x.MaCuaHang == MaCuaHang).FirstOrDefault();
